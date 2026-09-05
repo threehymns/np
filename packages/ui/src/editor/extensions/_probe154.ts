7593327f23558dce6c6425d987c2ee06a27db748
threehymns <70611435+threehymns@users.noreply.github.com>
2026-09-05 07:50:47 -0400

    chore: remove stray probe file

--- packages/ui/src/editor/extensions/_probe154.ts
+++
@@ -1,6 +0,0 @@
-import { EditorState } from "@codemirror/state";
-import { syntaxTree } from "@codemirror/language";
-import { markdown } from "@codemirror/lang-markdown";
-import { GFM } from "@lezer/markdown";
-const md = markdown({ extensions:[GFM] as any });
-function k(doc:string){ const s=EditorState.create({doc,extensions:[md]}); const out=[]; syntaxTree(s).iterate({enter:(n:any)=>{ if(/Blockquote|Paragraph/i.test(n.name)) out.push(n.name+":"+JSON.stringify(s.doc.sliceString(n.from,n.to))}}); \n; }); } catch(e){} }
